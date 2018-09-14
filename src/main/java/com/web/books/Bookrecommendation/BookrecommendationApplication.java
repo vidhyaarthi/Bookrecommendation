88faafbeb08a629a966dabf88d1d2252d64e3aa8
vidhyaarthi <32821589+vidhyaarthi@users.noreply.github.com>
2018-09-14 18:50:33 +0200

    Added maven project structure

---
+++ src/main/java/com/web/books/Bookrecommendation/BookrecommendationApplication.java
@@ -0,0 +1,12 @@
+package com.web.books.Bookrecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookrecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookrecommendationApplication.class, args);
+	}
+}
